5fff31275288f3d2f720af37931a14585fd0fa81
Juan Bezanilla <juanbezanillaz@gmail.com>
2022-08-23 12:03:31 -0400

    show is ready

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,2 +1,5 @@
 class BoatsController < ApplicationController
+  def show
+    @boat = Boat.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "boats/:id", to: "boats#show", as: :boat
 
   # Defines the root path route ("/")
   # root "articles#index"
