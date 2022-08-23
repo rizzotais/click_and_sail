9d993ba10778eab1932250e78210c67bc645efa5
Tais Rizzo <rizzotais@gmail.com>
2022-08-23 11:56:26 -0400

    change the db, create the new

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,2 +1,18 @@
 class BoatsController < ApplicationController
+  def new
+    @boat = Boat.new
+  end
+
+  def create
+    @boat = Boat.new(boat_params)
+    @boat.user = current_user
+    @boat.save
+    # redirect_to boat_path(@boat)
+  end
+
+  private
+
+  def boat_params
+    params.require(:boat).permit(:brand, :size, :boat_type, :year)
+  end
 end
---
+++ db/migrate/20220823150341_change_type_column.rb
@@ -0,0 +1,5 @@
+class ChangeTypeColumn < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :boats, :type, :boat_type
+  end
+end
